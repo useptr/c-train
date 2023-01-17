6126384021ff1edc7cfd3ccb6de7afa4cee4bdcf
Павел <wollow.gm@gmail.com>
2023-01-17 17:58:12 +0700

    scanf func

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
+#pragma warning( disable: 4996)
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	int num = 0;
-	printf("num = %d", num);
+	char str[128];
+	scanf("%s", str);
+	printf("str = %s\n", str);
+	int num;
+	scanf("%d", &num);
+	printf("num = %d\n", num);
+	/*printf("str length = %d\n", strlen(str));
+	printf("str length = %d\n", sizeof(str)/sizeof(str[0]));*/
 	return 0;
 }
