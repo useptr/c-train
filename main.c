cad46a4db3ecce61effb6f6a88f6d3a960dd1048
useptr <wollow.gm@gmail.com>
2023-01-18 18:17:12 +0700

    func in c lang

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #pragma warning( disable: 4996)
 #include <stdio.h>
 #include <string.h>
+void foo();
 int main()
 {
-	char str[128];
+	foo();
+	/*char str[128];
 	scanf("%s", str);
 	printf("str = %s\n", str);
 	int num;
 	scanf("%d", &num);
-	printf("num = %d\n", num);
+	printf("num = %d\n", num);*/
 	/*printf("str length = %d\n", strlen(str));
 	printf("str length = %d\n", sizeof(str)/sizeof(str[0]));*/
 	return 0;
+}
+void foo() {
+	printf("%s", "Hello n-w\n");
 }
